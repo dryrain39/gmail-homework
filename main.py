dd6d9a1695a1448ef6856725feb6bf5a1462e6b1
Geonu Ahn <akwoo1@naver.com>
2018-08-04 15:37:47 +0900

    v2

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from gmail import Gmail
 import json
 from bs4 import BeautifulSoup
+import re
+
+DEBUG = True
+
+
+def debug(text):
+    if DEBUG is True:
+        print(text)
 
 
 # Read configuration before start.
@@ -13,16 +21,32 @@ def read_config():
         exit("Config read error.")
 
 
+def get_mail(gid, pw, sender):
+    g = Gmail()
+    g.login(gid, pw)
+    mails = g.inbox().mail(sender=sender)
+    m = []
+    for mail in mails:
+        mail.fetch()
+        debug(mail.html)
+        m.append(mail.html)
+
+    g.logout()
+    return m
+
+
+def get_visible_text(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+    visible_text = soup.getText(strip=True)
+    return visible_text
+
+
+def parse_url(url_string):
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url_string)
+    return urls
+
+
 cfg = read_config()
-
-g = Gmail()
-g.login(cfg["account_user"], cfg["account_pass"])
-
-mails = g.inbox().mail(sender=cfg["mail_sender"])
-
-for mail in mails:
-    mail.fetch()
-    print(mail.body)
-
-
-g.logout()
+for i in get_mail(cfg["account_user"], cfg["account_pass"], cfg["mail_sender"]):
+    debug(parse_url(get_visible_text(i)))
