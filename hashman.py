5f1233d3a9595a3810a81d9f167904b08fa3be3f
Geonu Ahn <akwoo1@naver.com>
2018-08-07 21:30:48 +0900

    code beautify, change return(exclude sha 256)

--- hashman.py
+++ hashman.py
@@ -1,12 +1,14 @@
 import hashlib
 
+
 def hashall(inputfile):
     f = open(inputfile, 'rb')
     data = f.read()
     f.close()
 
-    r = {'md5': hashlib.md5(data).hexdigest(),
-         'SHA-1': hashlib.sha1(data).hexdigest(),
-         'SHA-256': hashlib.sha256(data).hexdigest()}
+    r = {
+        'MD5': hashlib.md5(data).hexdigest(),
+        'SHA1': hashlib.sha1(data).hexdigest()
+    }
 
     return r
