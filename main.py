4e306f4179512700b7e6517b8bd1b5d05ea715f8
Geonu Ahn <akwoo1@naver.com>
2018-08-07 21:34:54 +0900

    Revert "Revert "add stenographer(test code)""
    This reverts commit 7d1729d96225f618ba4c930f9840db38e5d088df.

--- main.py
+++ main.py
@@ -17,11 +17,10 @@ from PIL import Image
 
 import hashman
 
-
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def read_config():
@@ -92,7 +91,10 @@ def download_image(url, path):
                 f.write(chunk)
 
         logging.debug(filename + " download successful.")
-        return filename
+        return {
+            "filename": filename,
+            "full_url": r.url
+        }
 
     logging.error(url + " is not image or 404.")
     return False
@@ -101,27 +103,39 @@ def download_image(url, path):
 def downloader(url, path):
     logging.debug("start test url: " + url)
 
-    d_filename = False
+    d_data = False
 
-    while d_filename is False:
-        d_filename = download_image(url, path)
+    while d_data is False:
+        d_data = download_image(url, path)
 
         if len(url) is 0:
             logging.debug("failed.")
             return False
 
-        url = url[0:-1]
-        logging.debug("download Failed. retry: ")
+        if d_data is False:
+            url = url[0:-1]
+            logging.debug("download Failed. retry: ")
         pass
     else:
         logging.debug("Perfect!")
         pass
 
     logging.debug("ok. success.")
-    return d_filename
+    d_data["short_url"] = url
+    return d_data
 
 
 def postman(mail, time):
+    data = {
+        'Date': time.strftime('%Y-%m-%d %H:%M:%S'),
+        'Short URL': "",
+        'Full URL': "",
+        'Filename': False,
+        'Latitude': "N/A",
+        'Longitude': "N/A",
+        'MD5': None,
+        'SHA1': None
+    }
     dirname = cfg['image_dir'] + time.strftime('%Y-%m-%d')
     filename = False
     gps_data = 'N/A'
@@ -132,38 +146,49 @@ def postman(mail, time):
 
     # downloader
     for url in parse_url(get_visible_text(mail)):
-        filename = downloader(url, dirname)
+        downloader_result = downloader(url, dirname)
+
+        if downloader_result is False:
+            return None
+
+        data["Filename"] = downloader_result['filename']
+        data["Short URL"] = downloader_result['short_url']
+        data["Full URL"] = downloader_result['full_url']
 
     # gps_finder
-    if filename is not False:
-        try:
-            image = Image.open(dirname + '/' + filename)
-            gps_data = location_master.get_exif_data(image)
-        except Exception:
-            pass
-    else:
-        return
+    try:
+        image = Image.open(dirname + '/' + data["Filename"])
+        gps_data = location_master.get_exif_data(image)
+        data['Latitude'] = gps_data[0]
+        data['Longitude'] = gps_data[1]
+    except Exception:
+        pass
+
 
     # hashman
-    hash_data = hashman.hashall(dirname + '/' + filename)
+    hash_data = hashman.hashall(dirname + '/' + data["Filename"])
+    data["MD5"] = hash_data["MD5"]
+    data["SHA1"] = hash_data["SHA1"]
 
     # stenographer
+    print(data)
 
     # map_drawer
+
+    # watchmaker
 
     # treasure_hunter
 
     # print(mail)
-
 
     pass
 
 
 # download_image('http://fl0ckfl0ck.info/%E1%84%88%E1%85%A9%E1%84%88%E1%85%B5.jpg')
 
-gps_finder('./2018-08-03/druwa.jpg')
+# gps_finder('./2018-08-03/druwa.jpg')
 
-exit()
+# exit()
 global cfg
 cfg = read_config()
 mailbox = get_mail(cfg["account_user"], cfg["account_pass"], cfg["mail_sender"])
