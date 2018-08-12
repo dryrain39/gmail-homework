043980e77d2b4f4025faccc91d07dfaaeb294a82
Geonu Ahn <akwoo1@naver.com>
2018-08-12 13:23:38 +0900

    edit mapmaker

--- mapmaker.py
+++ mapmaker.py
@@ -1,15 +1,41 @@
 import requests
+import logging
 
 
 class Mapmaker:
-    locationArray = ['37.49848938888889,127.01871491666667', '37.52779008333333,126.90460966666667']
-    imageSize = '600x300'
+    imagePath = ''
+    locationArray = []
+    imageSize = '640x640'
+    center = None
+    zoom = None
+
+    def __init__(self, imgPath):
+        self.imagePath = imgPath
+        logging.debug('Mapmaker Loaded!')
+
+    def reform(self):
+        if self.center == 'None':
+            self.center = None
+
+        if self.zoom == 'None':
+            self.zoom = None
+
+    def reset(self):
+        self.locationArray = []
+        self.imageSize = '640x640'
+        self.center = None
+        self.zoom = None
 
     def add_item(self, item):
         self.locationArray.append(item)
 
     def draw_map(self):
-        url = 'https://maps.googleapis.com/maps/api/staticmap?size=' + self.imageSize + '0&maptype=roadmap'
+        print self.locationArray
+
+        ct = 'center=' + self.center + '&' if self.center is not None else ''
+        zm = 'zoom=' + self.zoom + '&' if self.zoom is not None else ''
+
+        url = 'https://maps.googleapis.com/maps/api/staticmap?' + ct + zm + 'size=' + self.imageSize
         for idx, loc in enumerate(self.locationArray):
             url = url + '&markers=color:red|label:' + str(idx + 1) + '|' + loc
 
@@ -18,16 +44,14 @@ class Mapmaker:
         for idx, loc in enumerate(self.locationArray):
             url = url + '|' + loc
 
+        print url
+
         r = requests.get(url)
         header = r.headers['content-type'].split('/')
         if header[0] == "image":
-            with open("timehere.jpg", 'wb') as f:
+            with open(self.imagePath + "all.jpg", 'wb') as f:
                 for chunk in r:
                     f.write(chunk)
 
-    def __init__(self):
-        pass
+        self.locationArray = []
 
-
-ps = Mapmaker()
-ps.draw_map()
