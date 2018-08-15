5768c79eaace65e44ebd6e64aec6a4e1bfe83cec
Geonu Ahn <akwoo1@naver.com>
2018-08-15 22:56:13 +0900

    update config & paste

--- paste.py
+++ paste.py
@@ -1,8 +1,8 @@
-json_f = """
-{
+json_f = """{
   "/": "## Account option ##",
   "account_user": "",
   "account_pass": "",
+  "selenium_pass": "",
 
   ".": "## Mail receive option ##",
   "mail_sender": "fl0ckfl0ck@hotmail.com",
@@ -10,12 +10,13 @@ json_f = """
   "_": "## file option ##",
   "csv_path": "./db.csv",
   "image_dir": "./images/",
+  "sel_image_dir": "./sel-images/",
   "map_preset": [
     ["AUTO", "None", "None"],
     ["Seoul", "37.1369993,126.7354759", "8"],
     ["Maynila","16.3611357,120.6427479", "7"]
   ],
-  "%": "Don't touch! > <)//",
+  "sel_driver_path": "./chromedriver_win32.exe",
   "debug_lvl": 2
 }
 """
